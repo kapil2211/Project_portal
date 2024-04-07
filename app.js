a953efbcf8ec2c1223a65ac6c17928d062f452be
Rajnesh <rajnesh34698@gmail.com>
2024-04-08 02:40:23 +0530

    added npm packages - ejs-mate,passport,passport-local,passport-local-mongoose also added sign up page and also created model for userschema

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
 
 let port=8080;
 
@@ -7,9 +8,56 @@ app.set("view engine","ejs");
 const path=require("path");
 app.set("views",path.join(__dirname,"/views"));
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const ejsMate=require("ejs-mate");
+app.engine("ejs",ejsMate);
+
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname,"public/css")));
 app.use(express.static(path.join(__dirname,"public/js")));
+
+const User=require("models/user.ejs");
+const passport=require("passport");
+const localStrategy=require("passport-local");
+
+
+main()
+.then((res)=>{
+    console.log("connection with database formed successfully");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/project_portal');
+};
+
+
+app.get("/signup",(req,res)=>{
+    res.render("users/signup.ejs");
+});
+
+app.post("/signup",async(req,res)=>{
+   try{
+    let{username,email,password}=req.body;
+    const newUser=new User({email,password});
+    const registeredUser=await User.register(newUser,password);
+    req.login(registeredUser,(err)=>{
+        if(err){
+            return nextTick(err);
+        }
+        res.redirect("/home.ejs");
+    });
+   }catch(err){
+    res.send("some error occured");
+   }
+
+});
+
+app.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
 
 app.use((req,res)=>{
     console.log("request received");
